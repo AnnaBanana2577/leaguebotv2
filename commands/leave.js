c25112419187af7cbae4c407c9bcdd7fc0b6962a
Anna <annabanana2577@outlook.com>
2022-03-18 17:57:21 -0400

    Bug Fixes

--- commands/leave.js
+++ commands/leave.js
@@ -3,8 +3,8 @@ const getNamesFromIds = require("../helpers/getNamesFromIds");
 
 module.exports = async (message) => {
     const nextMatch = getMatchDetails(matches[nMatch]);
-    const playerNames = await getNamesFromIds(queue);
-    const output = playerNames.join(' - ');
+    let playerNames = await getNamesFromIds(queue);
+    let output = playerNames.join(' - ');
 
     if (!queue.includes(message.author.id)){
         message.channel.send(`You have not joined a match yet.\n**${queue.length} Players**: ${output}`);
@@ -12,5 +12,7 @@ module.exports = async (message) => {
     }
 
     queue = queue.filter(function(ob){ return(ob !== message.author.id) });
+    playerNames = await getNamesFromIds(queue);
+    output = playerNames.join(' - ');
     message.channel.send(`<@${message.author.id}> has left the ${nextMatch.name} match\n**${queue.length} Players**: ${output}`);
 }
